954e5a464c6500e66d7780eb0d46e03e80aff35c
Vedant Verma <162734158+VedantVerma06@users.noreply.github.com>
2025-05-23 12:14:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const artworks = [
   { img: "art66.jpg", caption: "'Ken Takakura -<br>Okkarun'", instagram: "https://instagram.com/vedantartshttps://www.instagram.com/p/DBn86d2SOdi/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { img: "68.jpg", caption: "'Toya Todoroki -<br>DABI🔥'", instagram: "https://www.instagram.com/p/DCmaLTCyAn4/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { img: "art67.jpg", caption: "'The Subsitute Shinigami<br> Kurosaki Ichigo'", instagram: "https://www.instagram.com/p/DJYxc1tT9zL/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA===" },
-
+  { img: "art68.jpg", caption: "'The Shadow Monarch<br> Sung Jinwoo'", instagram: "https://www.instagram.com/p/DJYxc1tT9zL/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA===" },
 ];
 
 const galleryGrid = document.getElementById('galleryGrid');
